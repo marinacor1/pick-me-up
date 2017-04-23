ca72abcc73807c34f09d4993cf2d0a10373ac6d1
Marina Corona <marinaspam1234@gmail.com>
2017-04-23 12:24:54 -1000

    Initial factory for driver created

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -2,9 +2,9 @@ FactoryGirl.define do
   factory :driver do
     name Faker::Name.unique.name
     email Faker::Internet.email
-    initial_location ["Denver, CO", "Littleton, CO", "Colorado Springs, CO"]
-    destination []"Avon, CO", "Vail, CO", "Winter Park, CO"]
+    initial_location "Denver, CO"
+    destination "Avon, CO"
     date Time.now.strftime("%m/%d/%y")
-    time ["Morning", "Afternoon", "Evening"][rand(0..2)]
+    time "Morning"
   end
 end
